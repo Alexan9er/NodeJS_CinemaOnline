f53be31174571312a07ab831670cc553f78f3f5f
Alexander Kleymonov <Kleimionov.Alexander@gmail.com>
2019-11-22 09:22:46 +0300

    Invalid file structure removed.

--- controllers/index.js
+++
@@ -1,7 +0,0 @@
-const users = require("./users");
-const films = require("./films");
-
-module.exports = {
-  users,
-  films
-};
--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const userController = require("../controllers").users;
-
-module.exports = app => {
-  app.get("/api/users", userController.list);
-  app.post("/api/users", userController.create);
-  app.get("/api/users/:id", userController.userDetails);
-};
