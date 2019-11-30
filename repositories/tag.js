a7887bd18c3833585c23cb25c1867476caf3f047
Alexander Kleymonov <Kleimionov.Alexander@gmail.com>
2019-11-30 12:37:57 +0300

    Added feature to get tags by Ids.

--- repositories/tag.js
+++ repositories/tag.js
@@ -1,8 +1,19 @@
 const Tag = require("../models/tag");
 
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+
 class TagRepository {
   create(tag) {
     return Tag.create(tag);
+  }
+
+  getTagsByIds(tagsIds) {
+    return Tag.findAll({
+      where: {
+        id: { [Op.in]: tagsIds }
+      }
+    });
   }
 }
 
