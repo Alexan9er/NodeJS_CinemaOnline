8eea5f9ca3da1ef84edca8004c5addd302f26fc4
Alexander Kleymonov <Kleimionov.Alexander@gmail.com>
2019-12-02 16:01:24 +0300

    Added feature to get User with role.

--- repositories/user.js
+++ repositories/user.js
@@ -1,11 +1,19 @@
 const User = require("../models/user");
+const Role = require("../models/role");
 
 class UserRepository {
   create(user) {
     return User.create(user);
   }
   getAllUsers() {
-    return User.findAll();
+    return User.findAll({
+      include: [
+        {
+          model: Role,
+          attributes: ["id", "title"]
+        }
+      ]
+    });
   }
   getCurrentUser(conditions) {
     return User.findOne({ where: conditions });
