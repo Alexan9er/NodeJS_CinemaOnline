0d70c61ee3519b13f5193c60967d1388ab5b094e
Alexander Kleymonov <Kleimionov.Alexander@gmail.com>
2019-12-07 13:39:38 +0300

    Added not-found middlewares.

---
+++ src/middlewares/not-found.js
@@ -0,0 +1,3 @@
+module.exports = (req, res, next) => {
+  res.status(404).send("Not found");
+};
--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const app = require("express")();
 const loaders = require("./loaders");
 
+const notFound = require("./middlewares/not-found");
+
 exports.start = () => {
   loaders(app);
 
-  app.use((req, res, next) => {
-    res.status(404).send("Not found");
-  });
+  app.use(notFound);
 
   const PORT = process.env.PORT || 5000;
 
