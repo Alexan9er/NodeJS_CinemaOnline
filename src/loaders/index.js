a8f483aee67190cc082ee82fcfe8dff99ec31b88
Alexander Kleymonov <Kleimionov.Alexander@gmail.com>
2020-01-02 14:12:13 +0300

    Added errors handler.

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,9 +1,11 @@
 const initExpress = require("./express");
 const initRoutes = require("./routes");
 const initSession = require("./session");
+const initError = require("./errors");
 
 module.exports = app => {
   initExpress(app);
   initSession(app);
   initRoutes(app);
+  initError(app);
 };
