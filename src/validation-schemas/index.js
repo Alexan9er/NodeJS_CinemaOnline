de48126a6384b173266927f9a07e8eda9d627e0c
Alexander Kleymonov <Kleimionov.Alexander@gmail.com>
2020-01-03 11:51:42 +0300

    Added validation for film's route.

--- src/validation-schemas/index.js
+++ src/validation-schemas/index.js
@@ -1,11 +1,17 @@
 const id = require("./id.js");
+
 const userUpdate = require("./user-update");
 const userLogin = require("./user-login");
 const userRegistration = require("./user-registration");
+
+const filmUpdate = require("./film-update");
+const filmCreate = require("./film-create");
 
 module.exports = {
   id,
   userUpdate,
   userLogin,
-  userRegistration
+  userRegistration,
+  filmUpdate,
+  filmCreate
 };
