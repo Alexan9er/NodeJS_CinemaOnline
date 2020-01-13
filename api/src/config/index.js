65ffba2410becfd6a56c286af00dec5009e3ad32
Alexander Kleymonov <Kleimionov.Alexander@gmail.com>
2020-01-13 20:47:24 +0300

    Added rabbitMQ.

--- api/src/config/index.js
+++ api/src/config/index.js
@@ -15,5 +15,11 @@ module.exports = {
         underscored: true
       }
     }
+  },
+  rabbitMQ: {
+    url:
+      process.env.RABBITMQ_URL ||
+      "amqp://hovwehjy:Gm16-MuTDBO5jnwJnwCc2YQrgX_zYbRL@elephant.rmq.cloudamqp.com/hovwehjy",
+    logsQueue: "cinema-logs"
   }
 };
