636d47c1ddebe9f056f565052137ad449d7e4b1d
Alexander Kleymonov <Kleimionov.Alexander@gmail.com>
2020-02-05 18:55:00 +0300

    Made mail variables to constant.

--- mailer/src/config/constants.js
+++ mailer/src/config/constants.js
@@ -8,5 +8,11 @@ module.exports = {
   logTypes: {
     error: "error",
     info: "info"
+  },
+  email: {
+    user: "12445f07f0e579",
+    password: "fa427d3026094b",
+    subject: "Design Your Model S | Tesla",
+    from: "elonmusk@tesla.com"
   }
 };
